86ca287c89fad09649e5b189d312c6ccb14b101f
Quentin3819 <quentin.comelli@live.fr>
2020-01-28 15:09:31 +0000

    Mise a jour Paralax

--- Flower/js/script.js
+++ Flower/js/script.js
@@ -1,5 +1,17 @@
 new Glide(".glide", {
     type: 'carousel',
     startAt: 0,
-    perView: 3
+    perView: 3,
+    breakpoints: {
+      1024: {
+        perView: 1
+      }}
   }).mount();
+
+  var rellax = new Rellax('.rellax',{
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+  });
